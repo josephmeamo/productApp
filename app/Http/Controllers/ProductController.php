56ed7d6dcbd3909dbbdc46b3d8d3603fff800eda
josephmeamo <jpm6852@students.uc-bcf.edu.ph>
2026-02-20 21:47:07 +0800

    Fixed products view with Objective section and products table; updated route and controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = [
+            ['name' => 'Laptop', 'price' => 45000, 'description' => 'High performance laptop'],
+            ['name' => 'Smartphone', 'price' => 20000, 'description' => 'Latest Android smartphone'],
+            ['name' => 'Headphones', 'price' => 3000, 'description' => 'Noise cancelling headphones'],
+            ['name' => 'Smartwatch', 'price' => 8000, 'description' => 'Waterproof smartwatch'],
+        ];
+
+        return view('products', compact('products'));
+    }
 }

0256b6c841614c1401953077dceb2eb0c4c786ad
josephmeamo <jpm6852@students.uc-bcf.edu.ph>
2026-02-20 21:09:59 +0800

    Added products array in ProductController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,35 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = [
+            [
+                'name' => 'Laptop',
+                'price' => 45000,
+                'description' => 'High performance laptop'
+            ],
+            [
+                'name' => 'Smartphone',
+                'price' => 20000,
+                'description' => 'Latest Android smartphone'
+            ],
+            [
+                'name' => 'Headphones',
+                'price' => 3000,
+                'description' => 'Noise cancelling headphones'
+            ],
+            [
+                'name' => 'Smartwatch',
+                'price' => 8000,
+                'description' => 'Waterproof smartwatch'
+            ],
+        ];
+
+        return view('products', compact('products'));
+    }
 }
